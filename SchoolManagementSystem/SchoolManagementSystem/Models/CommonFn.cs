40ea5304087c7bae5c1c69dd694e5e7dd9de9d3a
Kai Pillay <kai.pillay@outlook.com>
2024-12-19 09:49:37 +0000

    Database Connection String
    I tidied up some issues with the title on admin home and added the connection string for the database

---
+++ SchoolManagementSystem/SchoolManagementSystem/Models/CommonFn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace SchoolManagementSystem.Models
+{
+    public class CommonFn
+    {
+        public class Commonfnx
+        {
+            private readonly string connString = ConfigurationManager.ConnectionStrings["SchoolSys"].ConnectionString;
+
+            public void Query(string query)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                        {
+                            cmd.ExecuteNonQuery();
+                            Console.WriteLine("Query executed successfully.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Query execution failed: {ex.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
