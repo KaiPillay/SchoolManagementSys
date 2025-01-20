1f511d2377d134b240343dbe37ecbd45511f7aa0
Kai Pillay <kai.pillay@outlook.com>
2025-01-20 15:47:05 +0000

    Teacher Subject
    This allows Teachers to be linked to a class and subject

---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/TeacherSubject.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="Teacher Subject Assignment" Language="C#" MasterPageFile="~/Admin/AdminMst.Master" AutoEventWireup="true" CodeBehind="TeacherSubject.aspx.cs" Inherits="SchoolManagementSystem.Admin.TeacherSubject" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container p-md-4 p-sm-4">
+        <h3 class="text-center">Assign Teacher to Class and Subject</h3>
+        
+        <div class="row mb-3">
+            <div class="col-md-4">
+                <label for="ddlTeacher">Select Teacher</label>
+                <asp:DropDownList ID="ddlTeacher" runat="server" CssClass="form-control" AutoPostBack="true">
+                    <asp:ListItem Text="Select Teacher" Value="0" />
+                </asp:DropDownList>
+            </div>
+
+            <div class="col-md-4">
+                <label for="ddlClass">Select Class</label>
+                <asp:DropDownList ID="ddlClass" runat="server" CssClass="form-control">
+                    <asp:ListItem Text="Select Class" Value="0" />
+                </asp:DropDownList>
+            </div>
+
+            <div class="col-md-4">
+                <label for="ddlSubject">Select Subject</label>
+                <asp:DropDownList ID="ddlSubject" runat="server" CssClass="form-control">
+                    <asp:ListItem Text="Select Subject" Value="0" />
+                </asp:DropDownList>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <asp:Button ID="btnAssignTeacher" runat="server" CssClass="btn btn-primary" Text="Assign Teacher" OnClick="btnAssignTeacher_Click" />
+            </div>
+        </div>
+
+        <br />
+
+        <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover table-bordered" AutoGenerateColumns="False" EmptyDataText="No Record To Display" DataKeyNames="Id" OnPageIndexChanging="GridView1_PageIndexChanging">
+            <Columns>
+                <asp:BoundField DataField="Name" HeaderText="Teacher" SortExpression="Name" />
+                <asp:BoundField DataField="ClassName" HeaderText="Class" SortExpression="ClassName" />
+                <asp:BoundField DataField="SubjectName" HeaderText="Subject" SortExpression="SubjectName" />
+                <asp:CommandField ShowDeleteButton="True" />
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/TeacherSubject.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class TeacherSubject : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadTeacherDropdown();
+                LoadClassDropdown();
+                LoadSubjectDropdown();
+                LoadAssignments();
+            }
+        }
+
+        // Load Teacher dropdown
+        private void LoadTeacherDropdown()
+        {
+            try
+            {
+                string query = "SELECT TeacherID, Name FROM Teacher";
+                DataTable dt = fn.Fetch(query);
+                ddlTeacher.DataSource = dt;
+                ddlTeacher.DataTextField = "Name";  // Correct field name
+                ddlTeacher.DataValueField = "TeacherID";
+                ddlTeacher.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+            }
+        }
+
+
+        // Load Class dropdown
+        private void LoadClassDropdown()
+        {
+            try
+            {
+                string query = "SELECT ClassID, ClassName FROM Class";
+                DataTable dt = fn.Fetch(query);
+                ddlClass.DataSource = dt;
+                ddlClass.DataTextField = "ClassName";
+                ddlClass.DataValueField = "ClassID";
+                ddlClass.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+            }
+        }
+
+        // Load Subject dropdown
+        private void LoadSubjectDropdown()
+        {
+            try
+            {
+                string query = "SELECT SubjectID, SubjectName FROM Subject";
+                DataTable dt = fn.Fetch(query);
+                ddlSubject.DataSource = dt;
+                ddlSubject.DataTextField = "SubjectName";
+                ddlSubject.DataValueField = "SubjectID";
+                ddlSubject.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+            }
+        }
+
+        // Load Teacher-Subject-Class Assignments
+        private void LoadAssignments()
+        {
+            try
+            {
+                string query = @"SELECT ts.Id, t.Name, c.ClassName, s.SubjectName 
+                                 FROM teachersubject ts
+                                 JOIN Teacher t ON ts.TeacherID = t.TeacherID
+                                 JOIN Class c ON ts.ClassID = c.ClassID
+                                 JOIN Subject s ON ts.SubjectID = s.SubjectID";
+
+                DataTable dt = fn.Fetch(query);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+            }
+        }
+
+        // Assign Teacher to Class and Subject
+        protected void btnAssignTeacher_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int teacherId = int.Parse(ddlTeacher.SelectedValue);
+                int classId = int.Parse(ddlClass.SelectedValue);
+                int subjectId = int.Parse(ddlSubject.SelectedValue);
+
+                // Insert Teacher-Subject-Class Assignment into the database
+                string query = "INSERT INTO teachersubject (TeacherID, ClassID, SubjectID) VALUES (@TeacherID, @ClassID, @SubjectID)";
+                string connectionString = ConfigurationManager.ConnectionStrings["SchoolSys"].ConnectionString;
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@TeacherID", teacherId);
+                    cmd.Parameters.AddWithValue("@ClassID", classId);
+                    cmd.Parameters.AddWithValue("@SubjectID", subjectId);
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                LoadAssignments();  // Reload the assignments grid
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+            }
+        }
+
+        // GridView paging event
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            LoadAssignments();
+        }
+
+        // GridView row deletion event
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+                string query = "DELETE FROM teachersubject WHERE Id = @Id";
+                string connectionString = ConfigurationManager.ConnectionStrings["SchoolSys"].ConnectionString;
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                LoadAssignments();  // Reload the assignments grid
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+            }
+        }
+    }
+}
