8e05599b7679bb00842c57ca08f7ec2dd69a8821
Kai Pillay <kai.pillay@outlook.com>
2025-01-20 18:50:31 +0000

    Teacher expenses

---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/Expense.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMst.Master" AutoEventWireup="true" CodeBehind="Expense.aspx.cs" Inherits="SchoolManagementSystem.Admin.Expense" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div style="background-image:url('../Images/bg3.jpg'); width:100%; height:1200px; background-repeat: no-repeat; background-size:cover; background-attachment:fixed;">
+    <div class="container p-md-4 p-sm-4">
+        <div>
+            <asp:Label ID="lblMsg" runat="server"></asp:Label>
+        </div>
+        <h3 class="text-center">Manage Expenses</h3>
+
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+            <div class="col-md-6">
+                <label for="ddlClass">Class</label>
+                <asp:DropDownList ID="ddlClass" runat="server" CssClass="form-control" required>
+                </asp:DropDownList>
+            </div>
+            <div class="col-md-6">
+                <label for="ddlSubject">Subject</label>
+                <asp:DropDownList ID="ddlSubject" runat="server" CssClass="form-control" required>
+                </asp:DropDownList>
+            </div>
+        </div>
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+            <div class="col-md-6">
+                <label for="txtChargeAmount">Charge Amount</label>
+                <asp:TextBox ID="txtChargeAmount" runat="server" CssClass="form-control" placeholder="Enter Charge Amount" required></asp:TextBox>
+            </div>
+        </div>
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+            <div class="col-md-6">
+                <label for="txtDetails">Details</label>
+                <asp:TextBox ID="txtDetails" runat="server" CssClass="form-control" placeholder="Enter details" required></asp:TextBox>
+            </div>
+        </div>
+        <asp:Label ID="lblStatus" runat="server" ForeColor="Black" />
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+            <div class="col-md-3 col-md-offset-2 mb-3">
+                <asp:Button ID="btnAddExpense" runat="server" CssClass="btn btn-primary btn-block" Text="Add Expense" OnClick="btnAddExpense_Click" />
+            </div>
+        </div>
+        <div class="row mb-3 mr-lg-5 ml-lg-5 mt-md-5">
+            <div class="col-md-12">
+                <asp:GridView ID="GridViewExpenses" runat="server" CssClass="table table-hover table-bordered" DataKeyNames="ExpenseId" AutoGenerateColumns="False"
+                EmptyDataText="No Record To Display" OnPageIndexChanging="GridViewExpenses_PageIndexChanging" Width="1786px" AllowPaging="true" PageSize="4"
+                OnRowDeleting="GridViewExpenses_RowDeleting">
+                    <Columns>
+                        <asp:BoundField DataField="ExpenseId" HeaderText="Expense ID" ReadOnly="True">
+                            <ItemStyle HorizontalAlign="Center" />
+                        </asp:BoundField>
+                        <asp:TemplateField HeaderText="Class">
+                            <ItemTemplate>
+                                <asp:Label ID="lblClass" runat="server" Text='<%# Eval("ClassName") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Subject">
+                            <ItemTemplate>
+                                <asp:Label ID="lblSubject" runat="server" Text='<%# Eval("SubjectName") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="ChargeAmount" HeaderText="Charge Amount" SortExpression="ChargeAmount" />
+                        <asp:BoundField DataField="Details" HeaderText="Details" SortExpression="Details" />
+                        <asp:CommandField ShowDeleteButton="True" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </div>
+</div>
+
+</asp:Content>
---
+++ SchoolManagementSystem/SchoolManagementSystem/Admin/Expense.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class Expense : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClasses();
+                GetSubjects();
+                LoadExpenses();
+            }
+        }
+
+        private void GetClasses()
+        {
+            try
+            {
+                string query = "SELECT ClassID, ClassName FROM Class";
+                DataTable dt = fn.Fetch(query);
+                ddlClass.DataSource = dt;
+                ddlClass.DataTextField = "ClassName";
+                ddlClass.DataValueField = "ClassID";
+                ddlClass.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "An error occurred while loading the class list: " + ex.Message;
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void GetSubjects()
+        {
+            try
+            {
+                string query = "SELECT SubjectID, SubjectName FROM Subject";
+                DataTable dt = fn.Fetch(query);
+                ddlSubject.DataSource = dt;
+                ddlSubject.DataTextField = "SubjectName";
+                ddlSubject.DataValueField = "SubjectID";
+                ddlSubject.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "An error occurred while loading the subject list: " + ex.Message;
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void LoadExpenses()
+        {
+            try
+            {
+                string query = "SELECT e.ExpenseId, c.ClassName, s.SubjectName, e.ChargeAmount, e.Details FROM Expense e " +
+                               "JOIN Class c ON e.ClassId = c.ClassID " +
+                               "JOIN Subject s ON e.SubjectId = s.SubjectID";
+                DataTable dt = fn.Fetch(query);
+                GridViewExpenses.DataSource = dt;
+                GridViewExpenses.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "An error occurred while loading expenses: " + ex.Message;
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+
+        protected void btnAddExpense_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["SchoolSys"].ConnectionString;
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string insertQuery = "INSERT INTO Expense (ClassId, SubjectId, ChargeAmount, Details) VALUES (@ClassId, @SubjectId, @ChargeAmount, @Details)";
+                    MySqlCommand cmd = new MySqlCommand(insertQuery, connection);
+                    cmd.Parameters.AddWithValue("@ClassId", ddlClass.SelectedValue);
+                    cmd.Parameters.AddWithValue("@SubjectId", ddlSubject.SelectedValue);
+                    cmd.Parameters.AddWithValue("@ChargeAmount", txtChargeAmount.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Details", txtDetails.Text.Trim());  // Add this line for the new Details field
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        lblStatus.Text = "Expense added successfully.";
+                        lblStatus.ForeColor = System.Drawing.Color.Green;
+                        LoadExpenses();
+                    }
+                    else
+                    {
+                        lblStatus.Text = "Failed to add expense.";
+                        lblStatus.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "An error occurred: " + ex.Message;
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+
+        protected void GridViewExpenses_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridViewExpenses.PageIndex = e.NewPageIndex;
+            LoadExpenses();
+        }
+
+        protected void GridViewExpenses_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                int expenseId = Convert.ToInt32(GridViewExpenses.DataKeys[e.RowIndex].Values["ExpenseId"]);
+
+                string connectionString = ConfigurationManager.ConnectionStrings["SchoolSys"].ConnectionString;
+                string deleteQuery = "DELETE FROM Expense WHERE ExpenseId = @ExpenseId";
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    MySqlCommand cmd = new MySqlCommand(deleteQuery, connection);
+                    cmd.Parameters.AddWithValue("@ExpenseId", expenseId);
+
+                    connection.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        lblStatus.Text = "Expense deleted successfully.";
+                        lblStatus.ForeColor = System.Drawing.Color.Green;
+                        LoadExpenses();
+                    }
+                    else
+                    {
+                        lblStatus.Text = "Failed to delete expense.";
+                        lblStatus.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "An error occurred while deleting the expense: " + ex.Message;
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
