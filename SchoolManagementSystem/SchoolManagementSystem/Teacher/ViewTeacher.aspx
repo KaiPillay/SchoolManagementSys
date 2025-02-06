efcb54bb5662a951727f01812e983d30a05836f8
Kai Pillay <kai.pillay@outlook.com>
2025-02-06 10:54:50 +0000

    View Teachers

---
+++ SchoolManagementSystem/SchoolManagementSystem/Teacher/ViewTeacher.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Teacher/TeacherMst.Master" AutoEventWireup="true" CodeBehind="ViewTeacher.aspx.cs" Inherits="SchoolManagementSystem.Teacher.ViewTeacher" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h3 class="text-center">View Teachers</h3>
+    <asp:Label ID="lblStatus" runat="server" ForeColor="Black"></asp:Label>
+
+    <asp:GridView ID="gvTeachers" runat="server" AutoGenerateColumns="False" CssClass="table table-striped">
+        <Columns>
+            <asp:BoundField DataField="TeacherId" HeaderText="Teacher ID" SortExpression="TeacherId" />
+            <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+            <asp:BoundField DataField="DOB" HeaderText="Date of Birth" SortExpression="DOB" />
+            <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+            <asp:BoundField DataField="Mobile" HeaderText="Mobile" SortExpression="Mobile" />
+            <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+            <asp:BoundField DataField="Address" HeaderText="Address" SortExpression="Address" />
+        </Columns>
+    </asp:GridView>
+</asp:Content>
---
+++ SchoolManagementSystem/SchoolManagementSystem/Teacher/ViewTeacher.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Web.UI;
+
+namespace SchoolManagementSystem.Teacher
+{
+    public partial class ViewTeacher : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadTeachers();
+            }
+        }
+
+        // Method to load all teachers into the GridView
+        private void LoadTeachers()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["SchoolSys"].ConnectionString;
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string selectQuery = "SELECT TeacherId, Name, DOB, Gender, Mobile, Email, Address FROM Teacher";
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(selectQuery, connection);
+                    DataTable teacherTable = new DataTable();
+                    dataAdapter.Fill(teacherTable);
+
+                    gvTeachers.DataSource = teacherTable;
+                    gvTeachers.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "An error occurred: " + ex.Message;
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
